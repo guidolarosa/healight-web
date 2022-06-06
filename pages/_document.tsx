5811c32b8a356a4077710c90cfb0e5a88884a758
guidolarosa <guido.glarosa@gmail.com>
2022-06-06 20:11:50 -0300

    Updated styles

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,10 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;400;600&display=swap" rel="stylesheet"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <style data-href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;400;600&display=swap">
+          @font-face{font-family:'Montserrat'};
+        </style>
       </Head>
       <body>
         <Main />
